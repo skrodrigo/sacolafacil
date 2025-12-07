f8ac491a474755595fcaac9281b583c06d65f918
skrodrigo <rodrigoa0987@gmail.com>
2025-12-07 18:32:52 -0300

    fix: auth import

--- backend/src/repositories/auth.repository.ts
+++ backend/src/repositories/auth.repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from './../common/prisma.js';
-import { Prisma, User } from '@/generated/prisma/client.js';
+import { Prisma, User } from './../generated/prisma/client.js';
 
 export const authRepository = {
   async findUserByEmail(email: string): Promise<User | null> {
